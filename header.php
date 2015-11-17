696137800f922ba84bedc7cc9e4df0b484f02d4a
wegdesign <rcommodaro@gmail.com>
2015-11-17 09:37:51 +0100

    Header

--- header.php
+++ header.php
@@ -1,51 +1,18 @@
-<!DOCTYPE html>
-<!--[if IE 6]>
-<html id="ie6" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if IE 7]>
-<html id="ie7" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if IE 8]>
-<html id="ie8" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if !(IE 6) | !(IE 7) | !(IE 8)  ]><!-->
-<html <?php language_attributes(); ?>>
-<!--<![endif]-->
-<head>
-<meta charset="<?php bloginfo('charset'); ?>" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-<title><?php
-/*
- * Print the <title> tag based on what is being viewed.
- */
-global $page, $paged;
-wp_title('|', true, 'right');
-
-// Add the blog name.
-bloginfo('name');
-
-// Add the blog description for the home/front page.
-$site_description = get_bloginfo('description', 'display');
-if ($site_description && (is_home() || is_front_page())) {
-	echo " | $site_description";
-}
-
-// Add a page number if necessary:
-if ($paged >= 2 || $page >= 2) {
-	echo ' | ' . sprintf(__('Page %s', 'musicplay'), max($paged, $page));
-}
-	?></title>
-<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-
-<?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
-<!--[if lt IE 9]>
-<script src="<?php echo THEME_URI; ?>/js/html5.js" type="text/javascript"></script>
-<![endif]-->
-
-<link rel="shortcut icon" href="" type="image/x-icon" />
-
-<?php wp_head(); ?>
-</head>
+<!-- Inizio Header -->
+<!doctype html>
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7]> <html lang="en"> <![endif]-->
+<!--[if IE 7]>    <html lang="en"> <![endif]-->
+<!--[if IE 8]>    <html lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en"> <!--<![endif]-->
+	<head>
+		<meta charset="<?php bloginfo('charset'); ?>" />
+		<meta name="viewport" content="width=device-width,initial-scale=1">
+		<title><?php 	bloginfo( 'name' ); ?></title>
+		<meta name="description" content="<?php  get_bloginfo('description', 'display'); ?>">
+		<link rel="shortcut icon" href="" type="image/x-icon" />
+		<?php wp_head(); ?>
+	</head>
 
 <body <?php body_class(); ?>>
 	<div id="boxed">
@@ -53,11 +20,11 @@ if ($paged >= 2 || $page >= 2) {
 			<div class="topbar">
 				<div class="inner">
 					<div class="topbar_left ">
-						
 					</div>
 					<div class="topbar_right">
 						<div id="social-icons">
 							<!-- social -->
+							social
 						</div>
 					</div>
 				</div><!-- /inner -->
