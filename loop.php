414f5726a6c9f738f1233680eb7133588c06ddce
wegdesign <rcommodaro@gmail.com>
2015-11-18 12:32:08 +0100

    Style

--- loop.php
+++ loop.php
@@ -10,27 +10,29 @@
 			<?php get_template_part( 'utils/includes/' . $format ); ?>
 			
 			<?php if( $format != 'link' && $format != 'quote' && $format != 'aside') { ?>
-				<h2 class="entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php esc_attr( get_the_title() ); ?>">
-				<?php the_title(); ?></a></h2>
-				<?php } ?>
-
-				<?php if ( get_option( 'atp_postmeta' ) != "on" ) { ?>
-				<div class="post-info">
-					<?php if( $format != 'aside' && $format != 'quote' ){?>
-					<?php echo atp_generator('postmetaStyle'); ?>
+				<div class="entry-content">
+					<h2 class="entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php esc_attr( get_the_title() ); ?>">
+					<?php the_title(); ?></a></h2>
 					<?php } ?>
-				</div><!-- .post-info -->
-				<?php } ?>
-				
-				<?php if ( $format != 'quote' ){ ?>
-				<div class="post-entry">
+	
+					<?php if ( get_option( 'atp_postmeta' ) != "on" ) { ?>
+					<div class="post-info">
+						<?php if( $format != 'aside' && $format != 'quote' ){?>
+						<?php echo atp_generator('postmetaStyle'); ?>
+						<?php } ?>
+					</div><!-- .post-info -->
+					<?php } ?>
 					
-					<?php the_excerpt(); ?>
+					<?php if ( $format != 'quote' ){ ?>
+					<div class="post-entry">
+						
+						<?php the_excerpt(); ?>
+						
+						<?php if ( $format != 'quote'   && $format != 'aside' ) { ?>
+						<a class="more-link" href="<?php the_permalink() ?>"><?php echo atp_localize($atp_readmoretxt, '', '');?></a>
+						<?php  } ?>
 					
-					<?php if ( $format != 'quote'   && $format != 'aside' ) { ?>
-					<a class="more-link" href="<?php the_permalink() ?>"><?php echo atp_localize($atp_readmoretxt, '', '');?></a>
-					<?php  } ?>
-				
+					</div>
 				</div>
 				<?php } ?>
 		</div>
